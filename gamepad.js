4f734a436d4a571c936fe1702477cb496acc374e
janiceshiu <contrepoint21@gmail.com>
2019-12-11 16:37:47 +0800

    buttons to add gamepad and iframes

---
+++ gamepad.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(event) { setup(); });
+
+function setup() {
+  document.getElementById("add-gamepad").addEventListener("click", function(){
+  });
+
+  document.getElementById("add-iframe").addEventListener("click", function(){
+  });
+}
