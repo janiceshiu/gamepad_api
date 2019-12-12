31127cc15e1641ce6c28e993b6ffdabb1184d424
Marcos Cáceres <marcos@marcosc.com>
2019-12-12 19:16:34 +1100

    refactor: move comments

--- src/GamepadService.js
+++ src/GamepadService.js
@@ -1,3 +1,8 @@
+// This singleton is for the entire user agent
+// GamepadService {
+//   maplike<Consumer, ConsumerInfo> consumerInfoMap;
+// };
+
 export class GamepadService {
   // we are going to implement this!
 }
