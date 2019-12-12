12cad84e8cf8f3ead86422e5bcf26872b18fec5c
Marcos Cáceres <marcos@marcosc.com>
2019-12-12 19:15:32 +1100

    Remove comments... moving to GamepadService.js

--- src/index.js
+++ src/index.js
@@ -16,18 +16,3 @@ function addIframe() {
   iframe.src = "";
   document.getElementById("iframes").appendChild(iframe);
 }
-
-// This singleton is for the entire user agent
-// let gamepadService = {
-//   connectedGamepads = [] // sequences of gamepadInfos
-// } GamepadService {
-//   maplike<Consumer, ConsumerInfo> consumerInfoMap;
-// };
-
-// Consumer is kinda Window and “environment settings object”
-
-// Create a consumer class.
-// Create buttons to dynamically add and remove gamepads (do any default setup here)
-// Create buttons to dynamically add and remove iframes (do any default setup here)
-// Associate gamepads and iframes
-// Send events to various gamepads and iframes
