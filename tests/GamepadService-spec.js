9884977bb236a44bc9c71bc27d7480cf21c46e25
janiceshiu <contrepoint21@gmail.com>
2019-12-16 15:01:44 +0800

    test that consumerBecomesActive sets a consumer's isActive to true

--- tests/GamepadService-spec.js
+++ tests/GamepadService-spec.js
@@ -1,7 +1,19 @@
+import { Consumer, consumerBecomesActive } from "../src/Consumer.js";
 import { GamepadService } from "../src/GamepadService.js";
 
 describe("GamepadService", () => {
   it("can be constructed", () => {
     expect(new GamepadService()).toBeTruthy();
+  });
+});
+
+describe("consumerBecomesActive()", () => {
+  it("activates a consumer", () => {
+    const service = new GamepadService();
+    const consumer = new Consumer();
+    consumerBecomesActive(service, consumer);
+    const info = service.consumerInfoMap.get(consumer);
+
+    expect(info.isActive).toBe(true);
   });
 });
