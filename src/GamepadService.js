ab041c3e3a64f5f1a79481b8a4cdea850d7bdfa8
janiceshiu <contrepoint21@gmail.com>
2019-12-16 11:53:09 +0800

    implement skeleton gamepad service from the spec

--- src/GamepadService.js
+++ src/GamepadService.js
@@ -1,8 +1,17 @@
-// This singleton is for the entire user agent
-// GamepadService {
-//   maplike<Consumer, ConsumerInfo> consumerInfoMap;
-// };
+/*
+The gamepad service monitors the system for device connections and
+disconnections and notifies gamepad consumers. The service also fetches the
+current state of connected gamepads.
+
+interface GamepadService {
+  maplike<Consumer, ConsumerInfo> consumerInfoMap;
+  sequence<GamepadInfo?> connectedGamepads;
+};
+*/
 
 export class GamepadService {
-  // we are going to implement this!
+  constructor() {
+    this.consumerInfoMap = new Map();
+    this.connectedGamepads = [];
+  }
 }
