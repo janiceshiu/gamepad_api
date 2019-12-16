4c01767b1870dd6470501ac87d4cc9102759ad60
janiceshiu <contrepoint21@gmail.com>
2019-12-16 11:53:21 +0800

    import GamepadService and create a new instance of it

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+// To start the gamepad service, the user agent MUST perform the following steps:
+import { GamepadService } from "GamepadService.js";
+
+// If |gamepadServiceState| is null:
+
+// NOTE: can be satisfied just having a new gamepad cos this is creating a html page, not creating a browser
+
+// Set |gamepadService| to a new {{GamepadService}}.
+const gamepadService = new GamepadService();
+
 // document.addEventListener("DOMContentLoaded", setup);
 
 // function setup() {
