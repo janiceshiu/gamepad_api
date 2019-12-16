ebbc8f779e6433a1b366b43a1dd80eafc26da1d0
janiceshiu <contrepoint21@gmail.com>
2019-12-16 11:52:04 +0800

    temporarily comment out gamepad and iframe setup

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-document.addEventListener("DOMContentLoaded", setup);
+// document.addEventListener("DOMContentLoaded", setup);
 
-function setup() {
-  document.getElementById("add-gamepad").addEventListener("click", addGamepad);
-  document.getElementById("add-iframe").addEventListener("click", addIframe);
-}
+// function setup() {
+//   document.getElementById("add-gamepad").addEventListener("click", addGamepad);
+//   document.getElementById("add-iframe").addEventListener("click", addIframe);
+// }
 
-function addGamepad() {
-  const gamepad = document.createElement("button");
-  gamepad.innerHTML = "button";
-  document.getElementById("gamepads").appendChild(gamepad);
-}
+// function addGamepad() {
+//   const gamepad = document.createElement("button");
+//   gamepad.innerHTML = "button";
+//   document.getElementById("gamepads").appendChild(gamepad);
+// }
 
-function addIframe() {
-  const iframe = document.createElement("iframe");
-  iframe.src = "";
-  document.getElementById("iframes").appendChild(iframe);
-}
+// function addIframe() {
+//   const iframe = document.createElement("iframe");
+//   iframe.src = "";
+//   document.getElementById("iframes").appendChild(iframe);
+// }
