89e06b975da93deb00da191d61fad30782c3f1df
janiceshiu <contrepoint21@gmail.com>
2019-12-17 15:14:36 +0800

    use consumer ID in iframe src doc

--- src/Consumer.js
+++ src/Consumer.js
@@ -27,7 +27,7 @@ export class Consumer extends HTMLIFrameElement {
     this.isActive;
     this.hasGesture = false;
     console.log("created");
-    this.srcdoc = `<h1>Hello Consumer!</h1>`;
+    this.srcdoc = `<h1>Hello Consumer ${id}!</h1><button class='activate-consumer-${id}'>Activate Consumer ${id}</button>`;
   }
   // TODO: disconnect/connect registration as per activation algorithm" or similar
   // possible function signatures could be `onGamepadConnected(gamepad)` and `onGamepadDisconnected(gamepad)`
