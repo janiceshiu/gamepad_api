39e125a17a70c854cbe8f0d8bea9fa8d12b970ad
janiceshiu <contrepoint21@gmail.com>
2019-12-17 15:18:53 +0800

    activate consumer when activate consumer button is clicked

--- src/index.js
+++ src/index.js
@@ -12,4 +12,20 @@ document.addEventListener("DOMContentLoaded", () => {
 function addConsumer() {
   const c = new Consumer();
   document.getElementById("consumers").appendChild(c);
+
+  addActivateConsumerListener(c);
+}
+
+function addActivateConsumerListener(consumer) {
+  consumer.addEventListener("load", () => {
+    consumer.contentWindow.document
+      .getElementsByClassName(`activate-${consumer.id}`)[0]
+      .addEventListener("click", () => {
+        activateConsumer(gamepadService, consumer);
+      });
+  });
+}
+
+function activateConsumer(gamepadService, consumer) {
+  consumerBecomesActive(gamepadService, consumer);
 }
