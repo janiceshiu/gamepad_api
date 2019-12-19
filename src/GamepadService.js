7651df000d139d4120a67ced78444d5406e9dd9b
janiceshiu <contrepoint21@gmail.com>
2019-12-19 13:42:39 +0800

    remove activateConsumer and deactivateConsumer
    todo: move parts of this into consumer.setActiveState(), reimplement registering with OS later

--- src/GamepadService.js
+++ src/GamepadService.js
@@ -3,36 +3,4 @@ export class GamepadService {
     this.consumers = new Set();
     this.connectedGamepads = [];
   }
-
-  activateConsumer(consumer) {
-    const { consumers } = this;
-    for (const c of consumers) {
-      if (c.isActive) {
-        // 1. Register with the operating system to receive notifications when gamepads are connected or disconnected.
-        // TODO: Actual registration
-        break;
-      }
-    }
-
-    if (!consumers.has(consumer)) {
-      consumers.add(consumer);
-    }
-  }
-
-  deactivateConsumer(consumer) {
-    const { consumers } = this;
-
-    let hasActiveConsumer = false;
-
-    for (const c of consumers) {
-      if (c.isActive) {
-        hasActiveConsumer = true;
-        break;
-      }
-
-      if (!hasActiveConsumer) {
-        // 1. TODO: Unregister with the operating system to no longer receive notifications with gamepads are connected or disconnected.
-      }
-    }
-  }
 }
