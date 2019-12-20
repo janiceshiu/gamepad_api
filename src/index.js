556021199d028f04437c3d58bdd20bbd01df8716
janiceshiu <contrepoint21@gmail.com>
2019-12-20 09:59:38 +0800

    remove currently-unused event listener

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 import { Consumer } from "./Consumer.js";
-import { GamepadService } from "./GamepadService.js";
-
-const gamepadService = new GamepadService();
 
 document.addEventListener("DOMContentLoaded", () => {
   const consumersDiv = document.getElementById("consumers");
   document.getElementById("add-consumer").addEventListener("click", () => {
-    const consumer = Consumer.attachConsumer(consumersDiv);
-
-    consumer.addEventListener("activestatechange", event => {
-      // do something;
-    });
+    Consumer.attachConsumer(consumersDiv);
   });
 });
