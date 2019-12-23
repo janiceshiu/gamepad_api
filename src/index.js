c9da0615f9ceadfd61ba7bf868cffb198c9d6376
janiceshiu <contrepoint21@gmail.com>
2019-12-23 23:35:04 +0800

    remove unused import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import { Consumer } from "./Consumer.js";
 import { Gamepad } from "./Gamepad.js";
-import { GamepadButton } from "./GamepadButton.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   const consumersDiv = document.getElementById("consumers");
