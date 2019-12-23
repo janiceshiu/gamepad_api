0f3bc635e6f0fe4dec31a008635af11fca5bdd1d
janiceshiu <contrepoint21@gmail.com>
2019-12-23 17:51:16 +0800

    add basic html and styling to a gamepad

--- src/Gamepad.js
+++ src/Gamepad.js
@@ -1,6 +1,23 @@
+let gamepadId = 0;
+
 export class Gamepad extends HTMLElement {
   constructor() {
     super();
+
+    const id = gamepadId++;
+
+    // setup the gamepad's front end
+    this.innerHTML = `
+      <style>
+        .gamepad {
+          border: 1px solid grey;
+          display: inline-block;
+          margin: 5px;
+        }
+      </style>
+      Hello gamepad ${id}!
+    `;
+    this.classList.add("gamepad");
   }
 
   static attachGamepad(htmlElement) {
